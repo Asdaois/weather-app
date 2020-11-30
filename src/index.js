8ee10917a9d2de8792e8793fdc2cf49e4d793af3
Jose Guevara <josexx1234@gmail.com>
2020-11-30 01:04:20 -0400

    get forecast weather

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
+async function filterData(data) {
+  const newData = {};
+  for (key in data) {
+    if (key == "weather" || key == "main" || key == "wind" || key == "coord") {
+      newData[key] = data[key];
+    }
+  }
+  return newData;
+}
 async function getWheater(cityName = "") {
   const apikey = "fa1e5b4bc8756a5da47e2ed86ac26e74";
-  const urlapi = "api.openweathermap.org/data/2.5/weather?";
+  const api = "api.openweathermap.org/data/2.5/weather?";
+  const apiForecast = "https://api.openweathermap.org/data/2.5/onecall?";
+
   try {
-    const response = await fetch(
-      `https://${urlapi}q=${cityName}&appid=${apikey}`,
+    const responseApi = await fetch(
+      `https://${api}q=${cityName}&appid=${apikey}&units=metric`,
       {
         mode: "cors",
       }
     );
-    console.log("clima");
-    const data = await response.json();
-    console.log(data);
+    const currenDataComplete = await responseApi.json();
+    const dataNecessary = await filterData(currenDataComplete);
+    const responseApiForecast = await fetch(
+      `${apiForecast}lat=${dataNecessary.coord.lat}&lon=${dataNecessary.coord.lon}&exclude=minutely,alerts&appid=${apikey}`,
+      {
+        mode: "cors",
+      }
+    );
+    const forecastDate = await responseApiForecast.json();
+    console.log(forecastDate);
+    console.log(dataNecessary);
   } catch (error) {
     console.error(error);
   }
@@ -20,4 +39,4 @@ async function getWheater(cityName = "") {
  * can take a location and return the weather data for that location. For now,
  * just console.log)( the information.)
  * */
-getWheater("San Felix");
+getWheater("London");
