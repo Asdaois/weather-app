57fbfaf10a021e79b8199b90e98b7976fe16d337
Jose Guevara <josexx1234@gmail.com>
2020-11-30 00:03:33 -0400

    Connect with the api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-console.log("hey");
-async function getWheater() {
+async function getWheater(cityName = "") {
+  const apikey = "fa1e5b4bc8756a5da47e2ed86ac26e74";
+  const urlapi = "api.openweathermap.org/data/2.5/weather?";
   try {
-    const api = "";
+    const response = await fetch(
+      `https://${urlapi}q=${cityName}&appid=${apikey}`,
+      {
+        mode: "cors",
+      }
+    );
+    console.log("clima");
+    const data = await response.json();
+    console.log(data);
   } catch (error) {
-    console.loge(error);
+    console.error(error);
   }
 }
+/*
+ * Write the functions that hit the API. You’re going to want functions that
+ * can take a location and return the weather data for that location. For now,
+ * just console.log)( the information.)
+ * */
+getWheater("San Felix");
